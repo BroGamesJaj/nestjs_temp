d3cebbc55c08d2b15cee5613b1b6f1ba8158a792
BroGames <hunempire11@gmail.com>
2024-09-16 14:18:58 +0200

    kész

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,10 +1,13 @@
 import { Controller, Get, Render } from '@nestjs/common';
 import { AppService } from './app.service';
-
+import { Quotes } from "./quotes";
+import { randomInt } from 'crypto';
 @Controller()
+
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  
   @Get()
   @Render('index')
   getHello() {
@@ -12,4 +15,38 @@ export class AppController {
       message: this.appService.getHello()
     };
   }
+  
+  @Get("/quotes")
+  @Render('quotes')
+  GetQuotes() {
+    return {
+      Quotes
+    }
+  }
+
+  @Get("/randomQuote")
+  @Render('randomQuote')
+  GetRandomQuote() {
+    return {
+      randomquote: Quotes.quotes[randomInt(1, Quotes.limit+1)]
+    } 
+  }
+
+  @Get("/topAuthors")
+  @Render('topAuthors')
+  GetTopAuthors(){
+    let authors = new Map<string,number>();
+    Quotes.quotes.forEach(element => {
+      if(authors.has(element.author)) {
+        authors.set(element.author,authors.get(element.author)+1)
+      } else{
+        authors.set(element.author,1)
+      }
+    });
+    authors = new Map([...authors.entries()].sort((a , b) => b[1] - a[1]));
+    return {
+      authors
+    }
+  }
+
 }
